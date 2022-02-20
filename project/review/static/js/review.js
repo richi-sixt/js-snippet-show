8fbf183fc7b417182855628429b38bf46688918f
richi-sixt <richi.sixt@gmail.com>
2022-02-20 07:29:33 +0100

    update

--- project/review/static/js/review.js
+++ project/review/static/js/review.js
@@ -51,3 +51,35 @@ window.addEventListener('DOMContentLoaded', function () {
   job.textContent = item.job;
   info.textContent = item.text;
 });
+
+// show person based on item
+
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+// show next person
+nextBtn.addEventListener('click', function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+// show prev person
+prevBtn.addEventListener('click', function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
+// show random person
+randomBtn.addEventListener('click', function () {
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson(currentItem);
+});
