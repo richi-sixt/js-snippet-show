246ccd022ce3678b1dcd9a051035a569c18a1e56
richi-sixt <richi.sixt@gmail.com>
2022-02-22 12:57:49 +0100

    clearing styles

--- project/colorflipper/static/js/color-flipper-hex.js
+++ project/colorflipper/static/js/color-flipper-hex.js
@@ -1,7 +1,6 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
-// f15025
 btn.addEventListener('click', function () {
   let hexColor = '#';
 
