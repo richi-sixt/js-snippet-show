e5d220f36b1a71de1db7d7da4b03528e14709ce3
richi-sixt <richi.sixt@gmail.com>
2022-02-25 02:14:48 +0100

    dynamic slider

--- project/sliders/sliders.py
+++ project/sliders/sliders.py
@@ -5,6 +5,12 @@ sliders_bp = Blueprint('sliders', __name__,
                           static_folder='static',
                           url_prefix="/sliders")
 
-@sliders_bp.route('/')
-def sliders():
-    return render_template('sliders/index.html', title="Sliders: one")
+
+@sliders_bp.route('/static-slider')
+def static_slider():
+    return render_template('sliders/static.html', title="Sliders: static")
+
+@sliders_bp.route('/dynamic-slider')
+def dynamic_slider():
+    return render_template('sliders/dynamic.html', title="Sliders: dynamic")
+
